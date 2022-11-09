ef27d61a430eb65aa3e298d21616a9f4181220c5
scho13 <91648580+scho13@users.noreply.github.com>
2022-11-08 21:19:51 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -9,26 +9,26 @@ window.addEventListener("load", function() {
 	console.log("Auto play is set to" + video.autoplay)
 });
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 	video.play();
 });
 
-document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video");
-	video.pause();
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Video Up");
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("Speed is " + video.playbackRate);
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Video Down");
 	video.playbackRate = video.playbackRate * 0.9;
-	console.log("Speed is " + video.playbackRate);
-});
-
-document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Speed Video Up");
-	video.playbackRate = video.playbackRate / 0.9;
 	console.log("Speed is " + video.playbackRate);
 });
 
@@ -39,14 +39,6 @@ document.querySelector("#skip").addEventListener("click", function() {
 		video.currentTime = 0
 	}
 	console.log("Current Location of the video is " + video.currentTime)
-});
-
-document.querySelector("#slider").addEventListener("click", function() {
-	console.log("The current value is", video.volume)
-	video.volume = this.value / 100 
-	let vol= document.querySelector("#volume").innerHTML = this.value + "%";
-	console.log("The current value is", video.volume)
-	console.log(document.querySelector("#volume"))
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
@@ -62,10 +54,18 @@ document.querySelector("#mute").addEventListener("click", function() {
 	}
 });
 
-document.querySelector("#vintage").addEventListener("click", function() {
-	video.classList.add("oldSchool");
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("The current value is", video.volume)
+	video.volume = this.value / 100 
+	let vol= document.querySelector("#volume").innerHTML = this.value + "%";
+	console.log("The current value is", video.volume)
+	console.log(document.querySelector("#volume"))
 });
 
 document.querySelector("#orig").addEventListener("click", function() {
 	video.classList.remove("oldSchool");
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
 });
